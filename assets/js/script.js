f9b7d997bfe10a6c193778aa7c6bbf25749d6ad5
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-02-02 20:06:42 -0300

    cambios en css y html, simplificando código y  agregando smooth scroll en js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-/* Habilitación de tooltips de Bootstrap 5 */
+// Habilitación de tooltips de Bootstrap 5
 //selecciona todos los elementos del documento que tienen "data-bs-toggle=tooltip" y los enlista
 var tooltipTriggerList = document.querySelectorAll(
   '[data-bs-toggle="tooltip"]'
@@ -23,7 +23,26 @@ $(window).scroll(function () {
   }
 });
 
-/* Ocultar y Mostrar el texto de las tarjetas al hacer click en el título de las mismas, todas por igual*/
+//  Ocultar y Mostrar el texto de las tarjetas al hacer click en el título de las mismas, todas por igual
 $(".card-title").click(function () {
   $(".card-text").toggle("slow");
+});
+
+//Smooth Scrool: Al hacer click en elementos con etiqueta "a" se desplaza suavemente a la posición del ID especificado
+$("a").click(function (event) {
+  //verifica si el atributo href referido como this.hash no está vacío
+  if (this.hash !== "") {
+    //previene el comportamiento predeterminado del enlace, es decir, navegar de manera directa al enlace
+    event.preventDefault();
+    //almacena el atributo href (this.hash) en una variable llamada hash
+    var hash = this.hash;
+    //anima un desplazamiento vertical (scrolltop) de html y body
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top,
+      },
+      //este valor especifica la duración de la animación en milisegundos, en este caso 3 segundos.
+      3000
+    );
+  }
 });
