53f3cdd27beb579810b91cdd7dcdf7d55dcc0b0c
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-02-02 00:46:13 -0300

    modificaciones y documentación en archivo js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,25 +9,31 @@ $(document).ready(function () {
   });
 
   /*Animación de smooth scroll para los <a> del navbar*/
-  $("a").click(function () {
-    let redirect = this.hash;
-    $("html,body").animate(
-      {
-        scrollTop: $(redirect).offset().top - 60,
-      },
-      1200
-    );
-  });
+  // $("a").click(function () {
+  //   let redirect = this.hash;
+  //   $("html,body").animate(
+  //     {
+  //       scrollTop: $(redirect).offset().top - 60,
+  //     },
+  //     1200
+  //   );
+  // });
 
+  // Cada vez que se desplace el navegador(se haga scroll en la ventana), se cambiará el fondo de la barra de navegación.
   $(window).scroll(function () {
+    //se selecciona el elemento con clase .navbar y se almacena en la variable "navegador"
     let navegador = $(".navbar");
+    //si se desplaza más de 100 píxeles desde la parte superior
     if ($(this).scrollTop() > 100) {
+      //cambia al color indicado
       navegador.css("background", "#0DCAF0");
+      //sino (si el desplazamiento es menor a 100 píxeles), se elimina el fondo de la barra de navegación
     } else {
       navegador.css("background", "none");
     }
   });
 });
+
 /* Ocultar y Mostrar el texto de las tarjetas al hacer click en el título de las mismas, todas por igual*/
 
 $(".card-title").click(function () {
